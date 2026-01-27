c7199126a35bd0d6fe08280268b2d1f629242ef5
anknighted <becyabbie@gmail.com>
2026-01-27 08:09:17 +0300

    chore: remove unnecessary import of flutter/material to fix analyzer

--- lib/features/home/landing/manage_expense/cubit/delete_expense_category_cubit.dart
+++ lib/features/home/landing/manage_expense/cubit/delete_expense_category_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:leadership/models/remote/failure.dart';
 import 'package:leadership/services/api/expense_categories_service.dart';
